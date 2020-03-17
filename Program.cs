831056be81f1faea01a2f791b0bc5607c6d0cdf9
Jonathan <jon.byrne27@btinternet.com>
2020-03-17 20:47:53 +0000

    Updated with comments

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace GitDiff
     
     class Program
     {
-        public static string FilePicker()
+        public static string FilePicker() //asks user to input the name of the file they want to use
         {
             Console.WriteLine("What is the file you would like to read? Please spell correctly");
             string answer = Console.ReadLine();
-            answer.Trim();
+            answer.Trim(); //removes any whitespaces
             return answer;
         }
-        public static string FileCompared(bool result, string file1, string file2)
+        public static string FileCompared(bool result, string file1, string file2) //compares if the two files are the same by taking in a boolean result
         {
             if (result == true)
             {
@@ -29,48 +29,48 @@ namespace GitDiff
         {
             string answer1 = FilePicker();
             string answer2 = FilePicker();
-            while (answer1 == answer2)
+            while (answer1 == answer2) //if the same file is given twice then the user will be asked to give a different file to compare
             {
-                answer1 = FilePicker();
+                Console.WriteLine("Please do not give the same file");
                 answer2 = FilePicker();
             }
-            string[] file1 = File.ReadAllLines(answer1);
-            string[] file2 = File.ReadAllLines(answer2);
-            int n1 = file1.Length;
+            string[] file1 = File.ReadAllLines(answer1); //reads in the first file the user typed in and stores it as a string array
+            string[] file2 = File.ReadAllLines(answer2); //reads in the second file the user typed in and stores it as a string array
+            int n1 = file1.Length; //get the length for both string arrays
             int n2 = file2.Length;
             bool filesEqual = true;
-            if (n1 != n2)
+            if (n1 != n2) //if the array lengths are different then the files must not be the same, so the bool is changed to false
             {
                 filesEqual = false;
             }
             else
             {
-                int i = 0;
+                int i = 0; //create to variables that will be used to incrament through both arrays
                 int j = 0;
 
                 while ((n1 > 0) || (n2 > 0))
                 {
 
-                    string a = file1[i].ToLower();
-                    string b = file2[j].ToLower();
-                    if (a != b)
+                    string a = file1[i];
+                    string b = file2[j]; //variable that is overwritten each iteration
+                    if (a != b) //if the element of both arrays are not the same at the same position, then the files must not have been the same, so the bool is false and we no longer need to check the files 
                     {
                         filesEqual = false;
                         break;
                     }
                     i++;
-                    j++;
+                    j++; //goes too the next element
                     n1--;
-                    n2--;
+                    n2--; //reduction for the while loop to work
                 }
             }
-            return FileCompared(filesEqual, answer1, answer2);
+            return FileCompared(filesEqual, answer1, answer2); //return the result of the bool being true or false
         }
 
         static void Main(string[] args)
         {
             bool filesFound = false;
-            while (filesFound == false)
+            while (filesFound == false) //exception handling will check if the files exist in the bin folder, and will continue to ask the user to give the correct files if the exception is raised
             {
                 try
                 {
